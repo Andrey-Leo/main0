560398018100844735f40a96d08cadf74724f494
Andrey-Leo <leopardair@gmail.com>
2024-08-26 12:13:46 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,5 @@
+the_of_completed_dz = '12'
+number_of_hours_spent = '1.5'
+course_name = 'Python'
+time_for_one_task = str(float(number_of_hours_spent) / int(the_of_completed_dz))
+print('Курс:', course_name, ', всего задач:', the_of_completed_dz, ', затрачено часов:', number_of_hours_spent, ', среднее время выполнения:', time_for_one_task, 'часа.')
