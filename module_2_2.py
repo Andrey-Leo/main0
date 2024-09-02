48d58049919227aa596263f83211b7157bce4408
Andrey-Leo <leopardair@gmail.com>
2024-09-02 12:44:10 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,14 @@
+first = input()
+second = input()
+third = input()
+set_ = {first, second, third}
+if len(set_) == 3:
+     print(0)
+elif len(set_) == 2:
+     print(2)
+else:
+     print(3)
+
+
+
+
