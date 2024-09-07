ad511f40cf5874f675f204f976d0d4b884ad09e6
Andrey-Leo <leopardair@gmail.com>
2024-09-07 13:07:35 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,13 @@
+def result(n):
+    password = ''
+    for i in range(1, n):
+        for j in range(2, n):
+            if j <= i:
+                continue
+            if n % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
+
+
+n = int(input('Введите число от 3 до 20: '))
+print('Пароль: ', result(n))
