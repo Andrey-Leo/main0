eae92fc8b3e98068c4dcf5ad7c52a18f9ceb8b8a
Andrey-Leo <leopardair@gmail.com>
2024-09-15 22:08:10 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,15 +1,14 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    str_number_0 = str_number.rstrip('0')
     first = int(str_number[0])
-    if int(str_number[-1]) == 0:
-        return first * get_multiplied_digits(int(str_number[1:-1]))
-    elif len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+    if len(str_number_0) > 1:
+        return first * get_multiplied_digits(int(str_number_0[1:]))
     else:
         return first
 
 
 result = get_multiplied_digits(40203)
-result_1 = get_multiplied_digits(420)
+result_1 = get_multiplied_digits(4200)
 print(result)
 print(result_1)
