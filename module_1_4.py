cca28e7119509b1bd95e5265b9b7d585d455539a
Andrey-Leo <leopardair@gmail.com>
2024-09-22 01:30:20 +0300

    Delete module_1_4.py

--- module_1_4.py
+++
@@ -1,6 +0,0 @@
-my_string = input() # Противотанковые группы «Днепр» уничтожили станцию РЭБ в районе Очакова
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string [0])
-print(my_string[-1])
