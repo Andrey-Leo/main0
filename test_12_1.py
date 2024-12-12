8a3609a0c4c5549831133b4b4a0b8abd1e001cea
Andrey-Leo <leopardair@gmail.com>
2024-12-12 08:01:43 +0300

    Add files via upload

--- test_12_1.py
+++ test_12_1.py
@@ -1,6 +1,18 @@
 import unittest
 
-from runner import Runner
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
 
 
 class TestRunner(unittest.TestCase):
