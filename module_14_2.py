de2d43974a5d57b52b10df1682cf7cd16a52afa4
Andrey-Leo <leopardair@gmail.com>
2025-01-06 12:28:50 +0300

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+conn = sqlite3.connect('not_telegram.db')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL
+)
+''')
+
+# for i in range(1, 11):
+#     j = i * 10
+#     c.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+#               (f'user{i}', f'example{i}@gmail.com', f'{j}', 1000))
+#
+# for i in range(1, 11, 2):
+#     c.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, i))
+#
+# for i in range(1, 11, 3):
+#     c.execute("DELETE FROM Users WHERE id = ?", (i,))
+#
+#
+# c.execute("SELECT username, email, age, balance FROM Users WHERE age !=?", (60,))
+# users = c.fetchall()
+# for user in users:
+#     print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+
+c.execute("DELETE FROM Users WHERE id = ?", (6,))
+
+conn.commit()
+
+c.execute("SELECT COUNT(*) FROM Users")
+total_users = c.fetchone()[0]
+
+c.execute("SELECT SUM(balance) FROM Users")
+all_balances = c.fetchone()[0]
+
+print(all_balances / total_users)
+
+conn.close()
