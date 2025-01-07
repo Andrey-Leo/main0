6b66e44ddee5830d33acb38165a85a3113e82d57
Andrey-Leo <leopardair@gmail.com>
2025-01-07 12:25:53 +0300

    Update module_14_3.py

--- module_14_3.py
+++ module_14_3.py
@@ -7,7 +7,7 @@ import asyncio
 
 from pyexpat.errors import messages
 
-api = '7787351381:AAGe4dCdYg5rNLtwy2x4mSuRiHShOVEn6jU'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
