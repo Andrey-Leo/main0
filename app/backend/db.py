0bcddb5a223604fe86d19d94a6048d67082a8d10
Andrey-Leo <leopardair@gmail.com>
2025-01-31 10:51:12 +0300

    Update db.py

--- app/backend/db.py
+++ app/backend/db.py
@@ -3,13 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = 'sqlite:///taskmanager.db'  # Путь к базе данных SQLite
+DATABASE_URL = 'sqlite:///taskmanager.db' 
 
-# Создаём движок для подключения к базе данных
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-# Создаём фабрику сессий для работы с базой данных
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Базовый класс для всех моделей SQLAlchemy
 Base = declarative_base()
