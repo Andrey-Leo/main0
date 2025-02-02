e111c039a141406656aa11cd1b5239630f2fd6fb
Andrey-Leo <leopardair@gmail.com>
2025-02-02 10:21:32 +0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import task, user
+from app.routers import task, user
 
 
 app = FastAPI()
@@ -10,5 +10,8 @@ app.include_router(user.router)
 @app.get("/")
 async def root():
     return {"message": "Welcome to Taskmanager"}
+
+
+
 
 
